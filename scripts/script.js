5ce8f817393cc809cfec0cc45e9dce34fe38d867
1xtend <kokitoki0504@gmail.com>
2022-11-20 10:48:16 +0100

    first commit

--- scripts/script.js
+++ scripts/script.js
@@ -1,25 +1,5 @@
 import images from './images.js';
 
-/*
-
-<li class="gallery__item">
-  <a
-    class="gallery__link"
-    href="https://cdn.pixabay.com/photo/2010/12/13/10/13/tulips-2546_1280.jpg"
-  >
-    <img
-      class="gallery__image"
-      src="https://cdn.pixabay.com/photo/2010/12/13/10/13/tulips-2546__340.jpg"
-      data-source="https://cdn.pixabay.com/photo/2010/12/13/10/13/tulips-2546_1280.jpg"
-      alt="Tulips"
-    />
-  </a>
-</li>
-
-*/
-
-// Functions
-
 const gallery = document.querySelector('.js-gallery');
 const modal = document.querySelector('.js-lightbox');
 const modalImage = modal.querySelector('.lightbox__image');
